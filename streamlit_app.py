8338d9b86a8fc7a7f4df11f939c25d03c5758e6f
Sérgio Ricardo <working7.sergio@gmail.com>
2025-04-14 03:06:12 +0000

    add

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
+import numpy as np 
+import pandas as pd
 
-st.title("🎈 My new app")
+st.title(" UM TESTE SIMPLES")
+dataframe = pd.DataFrame(
+    np.random.randn(10,20),
+    columns=("col %d" % i for i in range(20)))
+
+st.dataframe(dataframe)
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
