8ae782fae2ecb7fcc45a7ab3730b1c388f19d9bc
Sérgio Ricardo <working7.sergio@gmail.com>
2025-04-14 03:41:37 +0000

    novo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,14 @@
 import streamlit as st
-add_selectbox = st.sidebar.selectbox(
-    'O quanto você gosta de estar conectado?',
-    ('Email','Home phone', 'Mobile phone')
+import time
 
-)
+latest_iteration = st.empty()
+bar = st.progress(0)
 
-add_slider = st.sidebar.slider(
-    'select a range of values', 
-    0.0, 100.0, (25.0, 75.0)
-)
+for i in range(1000):
+    latest_iteration.text(f'Iteration {i + 1}')
+    
+    # Progresso proporcional ao total (escala de 0 a 100)
+    progress = int((i + 1) / 1000 * 100)
+    bar.progress(progress)
+    
+    time.sleep(0.01)  # pode diminuir o tempo pra não demorar tanto
